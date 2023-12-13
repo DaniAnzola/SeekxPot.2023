9ae4929c798d83a87ee90de7d974438cbce5992a
Loeches <daniel27anzola@gmail.com>
2023-12-13 14:16:01 +0100

    Boton de ir a subir un garito

--- app/src/main/java/com/project/seekxpot/SubirGarito.java
+++ app/src/main/java/com/project/seekxpot/SubirGarito.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class SubirGarito extends AppCompatActivity {
 
+    //TODO RECIBO LA DIRECCION Y PONERLA EN UN TEX VIEW EN EL XML PERO QUE DEJE CONFIRMADA LA DIRRECION
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
